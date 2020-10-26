22ac289ec41b1bd3f0bf2cef7f048e6b71829ea8
Vithor <vithort@gmail.com>
2020-10-26 20:44:32 -0300

    Criando o Script de Movimento do Jogador
    Criando o Script de Movimento do Jogador

---
+++ Nave_2D/Assets/Scripts/Jogador.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jogador : MonoBehaviour
+{
+    private float moverHorizontal;
+    private float moverVertical;
+    private Vector2 mover;
+    private Rigidbody2D rb2d;
+    [SerializeField]
+    private float velocidade;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        moverHorizontal = Input.GetAxis("Horizontal");
+        moverVertical = Input.GetAxis("Vertical");
+
+        // Debug.Log(moverHorizontal);
+
+        mover = new Vector2(moverHorizontal, moverVertical);
+        rb2d.velocity = mover * velocidade;
+
+        // Debug.Log(rb2d.velocity);
+    }
+}
