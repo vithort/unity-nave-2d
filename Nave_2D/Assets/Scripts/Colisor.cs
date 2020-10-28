ff2518aa9ef65f6631d6741bc4e247f914e95f17
Vithor <vithort@gmail.com>
2020-10-27 21:22:31 -0300

    Destruindo Objetos com Colisor - BoxCollider2D

---
+++ Nave_2D/Assets/Scripts/Colisor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Colisor : MonoBehaviour
+{
+
+    private BoxCollider2D colisor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        colisor = GetComponent<BoxCollider2D>();
+        Vector3 posicao = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
+        float posicaoX = posicao.x;
+        float posicaoY = posicao.y;
+        // Debug.Log(posicao);
+        colisor.size = new Vector2(posicaoX, posicaoY) * 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerExit2D(Collider2D outro)
+    {
+        Destroy(outro.gameObject);
+    }
+}
