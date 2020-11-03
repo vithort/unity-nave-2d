8593a3ccf55017a4412278ff291125d51c6e3f37
Vithor <vithort@gmail.com>
2020-11-03 10:08:28 -0300

    Criando Efeito de Explosão
    Criando Efeito de Explosão

---
+++ Nave_2D/Assets/Scripts/Explosao.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosao : MonoBehaviour
+{
+
+    [SerializeField]
+    private float tempo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(GetComponent<AudioSource>() != null)
+            GetComponent<AudioSource>().Play();
+            Destroy(gameObject, tempo);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
