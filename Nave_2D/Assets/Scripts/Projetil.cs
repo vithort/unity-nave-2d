30c8fc7e9d1c568ebc7dbc58dccecfe8539da139
Vithor <vithort@gmail.com>
2020-11-03 15:04:54 -0300

    Criando o Inimigo
    Criando o Inimigo

---
+++ Nave_2D/Assets/Scripts/Projetil.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projetil : MonoBehaviour
+{
+    [SerializeField]
+    private float velocidade;
+    private Rigidbody2D rb2d;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        rb2d.velocity = transform.up * velocidade;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
